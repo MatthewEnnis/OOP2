77cf1e4be13ed54c283d50baead5870eb645d049
MatthewEnnis <mattennis123@gmail.com>
2020-04-14 22:21:38 +0100

    Updated GUI
    The GUI has been improved a fair bit but is still pretty unfinished. No added functionality.

--- Assignment/GUI.java
+++ Assignment/GUI.java
@@ -1,44 +1,112 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
+
 import java.util.Random;
 
 import java.awt.BorderLayout;
-import java.awt.CardLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.Color;
 
 public class GUI extends JFrame
 {
-	JTabbedPane tabs;
-	JPanel trainingPanel, testingPanel;
-	JLabel heading;
+	JPanel mainPanel, headingPanel, leftPanel, symptomPanel, dataPanel, testPanel, selectionPanel;
+	JLabel heading, tempLabel;
+	JComboBox tempSelect;
+	JCheckBox achesBox, coughBox, throatBox, dangerBox;
+	JButton testSymptoms;
 	
 	public GUI()
 	{
 		super("COVID-19 Diagnostic Tool");
 		
-		setVisible(true);
-		setSize(600,600);
-		setLayout(new BorderLayout());
+		System.out.println(javax.swing.UIManager.getDefaults().getFont("Label.font"));
 		
-		tabs = new JTabbedPane();
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 		
-		trainingPanel = new JPanel();
-		testingPanel = new JPanel();
+		setSize(640,360);
+		setLayout(new BorderLayout()); //BorderLayout so I can have the heading at the top
 		
-		tabs.addTab("Read data",trainingPanel);
-		tabs.addTab("Test symptoms",testingPanel);
+		//Heading
+		headingPanel = new JPanel();
 		
 		heading = new JLabel("COVID-19 Diagnostic Tool");
-		heading.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		heading.setFont(new Font("Dialog", Font.PLAIN, 20));
 		
-		add(heading,BorderLayout.NORTH);
-		add(tabs,BorderLayout.CENTER);
+		headingPanel.add(heading);
+		
+		//Main panel
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+		
+		//Adding them to borderlayout
+		add(headingPanel,BorderLayout.NORTH);
+		add(mainPanel,BorderLayout.CENTER);
+		
+		//Three main panels, data and test on the left and symptom on the right
+		leftPanel = new JPanel();
+		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+		
+		dataPanel = new JPanel();
+		dataPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Input training data"));
+		
+		testPanel = new JPanel();
+		testPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Test classifier accuracy"));
+		
+		leftPanel.add(dataPanel);
+		leftPanel.add(testPanel);
+
+		symptomPanel = new JPanel();
+		symptomPanel.setLayout(new BorderLayout());
+		symptomPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Test symptoms"));
+
+		mainPanel.add(leftPanel);
+		mainPanel.add(symptomPanel);
+		
+		//Symptom panel
+		tempLabel = new JLabel("Temperature: ");
+		tempSelect = new JComboBox<>(new String[] {"Hot","Normal","Cool"});
+		tempSelect.setSelectedIndex(1);
+		
+		
+		selectionPanel = new JPanel();
+		selectionPanel.setLayout(new GridLayout(3,2));
+		
+		achesBox = new JCheckBox("Aches");
+		coughBox = new JCheckBox("Cough");
+		throatBox = new JCheckBox("Sore throat");
+		dangerBox = new JCheckBox("Recently travelled to danger zone");
+		
+		testSymptoms = new JButton("Test symptoms");
+		
+		selectionPanel.add(achesBox);
+		selectionPanel.add(coughBox);
+		selectionPanel.add(throatBox);
+		selectionPanel.add(dangerBox);
+		selectionPanel.add(tempLabel);
+		selectionPanel.add(tempSelect);
+		symptomPanel.add(selectionPanel,BorderLayout.NORTH);
+		symptomPanel.add(testSymptoms,BorderLayout.SOUTH);
+		
+		
+		setVisible(true);
 	}
 }
